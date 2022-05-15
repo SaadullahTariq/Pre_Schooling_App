fc075ce1246ff6bc786c7dfeae79d292f6ac7bd4
SaadullahTariq <saadullahtariqoffical@gmail.com>
2022-05-15 12:43:04 +0500

    Drawing Section Started!!!

---
+++ app/src/main/java/com/example/fyp_preschool/DrawingActivity.java
@@ -0,0 +1,14 @@
+package com.example.fyp_preschool;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DrawingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawing);
+    }
+}
--- app/src/main/java/com/example/fyp_preschool/LandingActivity.java
+++ app/src/main/java/com/example/fyp_preschool/LandingActivity.java
@@ -10,7 +10,7 @@ public class LandingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        ImageView leaningView, quizView, gameView, videolearningView;
+        ImageView leaningView, quizView, gameView, videolearningView, drawingView;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
         setTitle(getString(R.string.landing_title));
@@ -18,10 +18,15 @@ public class LandingActivity extends AppCompatActivity {
         quizView = findViewById(R.id.quizView);
         gameView = findViewById(R.id.gameView);
         videolearningView = findViewById(R.id.videolearningView);
+        drawingView = findViewById(R.id.drawingView);
 
         leaningView.setOnClickListener(view -> startActivity(new Intent(LandingActivity.this, LearningActivity.class)));
 
         quizView.setOnClickListener(view -> startActivity(new Intent(LandingActivity.this, QuizActivity.class)));
+
+        drawingView.setOnClickListener(view -> { startActivity(new Intent(LandingActivity.this, DrawingActivity.class));
+
+        });
 
 
     }
